df6a365fddb51088318d4243a5b6b864efd49e8b
nemedinar <nemedinar@ut.edu.co>
2022-07-17 11:25:35 -0500

    primer-proyecto

---
+++ primer-proyecto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace primer_proyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
